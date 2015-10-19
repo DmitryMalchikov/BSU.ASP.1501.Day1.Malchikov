3460596e342db2346e01b19d15c0ea1cce3cbc21
MalchikovDmitry <dimamal1995@gmail.com>
2015-10-19 11:23:34 +0300

    Changes in RootFinder

--- BSU.ASP.1501.Day1.Malchikov/RootNth/RootFinder.cs
+++ BSU.ASP.1501.Day1.Malchikov/RootNth/RootFinder.cs
@@ -8,16 +8,23 @@ namespace RootNth
 {
     public class RootFinder
     {
-        public static double FindRoot(double number, double pow, double accuracy)
+        public static double FindRoot(double number, int pow, double accuracy)
         {
+            if (number < 0)
+                throw new ArgumentException();
+            if (number == 0) return 0;
+            if (pow <= 0)
+                throw new ArgumentException();
             if (pow == 1) return number;
+
             
             double xn = 0;
             for (double x = number / 2; ; x = xn )
             {
-                xn = (1 / pow) * ((pow - 1) * x + number / Math.Pow(x, pow - 1));
+                xn = (1.0 / pow) * ((pow - 1) * x + number / Math.Pow(x, pow - 1));
                 if (Math.Abs(x - xn) < accuracy) break;
             }
+
             return xn;
 
         }
